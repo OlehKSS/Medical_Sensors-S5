02664a1c150cb5b2950ee9ddb1f9c7125d6d5486
OlehKSS <olehkss@github.com>
2017-11-09 15:56:08 +0200

    Review improvements

--- _shared/phase_image.py
+++ _shared/phase_image.py
@@ -22,8 +22,8 @@ class PhaseImage:
         '''
 
         if (self._data is None):
-            #creates array for data, addional two rows and two cols added, for more convinient work with borders
-            self._data = np.zeros((self._rows + 2, self._cols + 2), dtype = 'uint16')
+            #creates array for data
+            self._data = np.zeros((self._rows, self._cols), dtype = 'uint16')
 
             with open(self._path, 'rb') as binary_file:
                 binary_file.seek(self._header_size)
@@ -35,7 +35,7 @@ class PhaseImage:
                     data_view[bytes_row_index : bytes_row_index + 2 * self._cols])
 
                     for (j, value) in enumerate(row_temp):
-                        self._data[i+1, j+1] = value[0]                          
+                        self._data[i, j] = value[0]                          
 
             return self._data
         else:
