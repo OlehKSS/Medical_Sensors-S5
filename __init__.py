c4af3dc991f5ffcf31d8eb658e680a6a6893649d
Brianna Burton <briannaburton@gmail.com>
2018-01-08 16:52:42 +0100

    fixes to linear regression

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from _shared.phase_image import PhaseImage
 from skimage import filters
 from region_growing_linear_est.quality_maps import quality_map_second_order
 from math import sqrt
+from region_growing_linear_est.linear_regression import linear_reg
 from particle_swarm_optimization.particle_initialization import phase_derivative_variance, threshold
 from particle_swarm_optimization.particle_operations import find_polarity_arrays
 from particle_swarm_optimization.find_residues import calculate_residues
@@ -30,48 +31,54 @@ for dirName, subdirList, fileList in os.walk(path):
             fileNames.append(fileName)
             filePaths.append(os.path.join(dirName, fileName))
 
-#print("Byte order", sys.byteorder)
-#print("Number of files found is {0}. Which would you like to open?".format(
-#    len(filePaths)))
+print("Byte order", sys.byteorder)
+print("Number of files found is {0}. Which would you like to open?".format(
+    len(filePaths)))
 
-#for index, fileName in enumerate(fileNames):
-#    print("{0} -> {1}".format(index, fileName))
+for index, fileName in enumerate(fileNames):
+    print("{0} -> {1}".format(index, fileName))
 
-#fileNo = int(input())
+fileNo = int(input())
 
 #reading binary file
-#phase_img = PhaseImage(256, 256, path = filePaths[fileNo])
-#data = phase_img.read()
+phase_img = PhaseImage(256, 256, path = filePaths[fileNo])
+data = phase_img.read()
 #pyplot.imshow(data, cmap = cm.Greys_r)
 #pyplot.show()
 
 
-image = io.imread('test_heart_image.jpg',as_grey = True)
+#image = io.imread('test_heart_image.jpg',as_grey = True)
 #stuff for finding the phase derv map
-phasemap = phase_derivative_variance(image)
-binary = threshold(phasemap)
+#phasemap = phase_derivative_variance(data)
+#binary = threshold(phasemap)
 
 #calculating the residues
-residue_map = calculate_residues(image)
+#residue_map = calculate_residues(data)
 
 #calculating the residues
 #[S1, S0, U1, U0] = find_polarity_arrays(binary,residue_map)
 
 #Set values to parameters of dPSO
-T = 1000    #maximal iteration times
-c1 = 2      #learning factor 1
-c2 = 2      #learning factor 2
+#T = 1000    #maximal iteration times
+#c1 = 2      #learning factor 1
+#c2 = 2      #learning factor 2
 
-fig = pyplot.figure()
-pyplot.gray()
-ax1 = fig.add_subplot(221)  # left side
-ax2 = fig.add_subplot(222)  # right side
-ax3 = fig.add_subplot(223)
-ax4 = fig.add_subplot(224)
-ax1.imshow(image)
-ax2.imshow(phasemap)
-ax3.imshow(binary)
-ax4.imshow(residue_map)
-pyplot.show()
+#fig = pyplot.figure()
+#pyplot.gray()
+#ax1 = fig.add_subplot(221)  # left side
+#ax2 = fig.add_subplot(222)  # right side
+#ax3 = fig.add_subplot(223)
+#ax4 = fig.add_subplot(224)
+#ax1.imshow(data)
+#ax2.imshow(phasemap)
+#ax3.imshow(binary)
+#ax4.imshow(residue_map)
+#pyplot.show()
 
+binary = numpy.zeros((256,256))
+binary[244,100] = 1
+phi_unwr_data = 100*numpy.ones((256,256))
+phi_unwr_data[244][100] = data[244][100]
+unwrapped_value = linear_reg(binary, phi_unwr_data, data, (245,100), 7)
+print(unwrapped_value)
 
--- region_growing_linear_est/linear_regression.py
+++ region_growing_linear_est/linear_regression.py
@@ -4,50 +4,47 @@ from math import pi
 
 '''Unwrap the phase using the linear regression prediction'''
 
-#Define a linear model phi_unwr = X_unwr * B + E where
-#phi_unwr is given and X_unwr, B, and E are calculated
-#Provide the unwrapped unwrapped phase data (phi_unwr), the
-#original image data, the coordinate to unwrap (point(i,j)),
-#and the window_size. It returns the unwrapped phase prediction.
+#Define a linear model unwr_im = X_unwr * B + E where unwr_im and X_unwr are given and B and E are calculated. Provide the binary checked image (indicating which pixels have been unwrapped (0 wrapped, 1 unwrapped), the unwrapped phase image (unwr_im), the original image data (wr_im), the coordinate to unwrap (point(i,j)), and the window_size. It returns the unwrapped phase prediction.
 
-#Initialize phi_unwr_data with some values not in the phi_unwr_data set (100 or something like that. Change value in line 32 if necessary)
-def linear_reg(phi_unwr_data, original_im_data, point, window_size):
+def linear_reg(checked_binary_im, unwr_im, wr_im, point, window_size):
     
     #Find X_unwr (window over original data) from the unwrapped phase data, the coordinate
     #to unwrap and window size
-    #[rows,cols] = numpy.shape(phi_unwr)
     low_range = int((window_size-1)/2)
     high_range = int((window_size+1)/2)
     
-    #Pad both arrays equal to high_range
-    phi_unwr_data = numpy.pad(phi_unwr_data, high_range, 'constant', constant_values = 100)
-    original_im_data = numpy.pad(original_im_data, high_range, 'constant', constant_values = 0)
+    #Pad the three arrays with size equal to high_range
+    checked_binary_im = numpy.pad(checked_binary_im, high_range, 'constant', constant_values = 0)
+    unwr_im = numpy.pad(unwr_im, high_range, 'constant', constant_values = 0)
+    wr_im = numpy.pad(wr_im, high_range, 'constant', constant_values = 0)
     
     #Windows of original and unwrapped data size window_size x window_size
-    X_unwr = original_im_data[point[0]-low_range+high_range:point[0]+2*high_range,point[1]-low_range+high_range:point[1]+2*high_range]
-    phi_unwr_data = phi_unwr_data[point[0]-low_range+high_range:point[0]+2*high_range,point[1]-low_range+high_range:point[1]+2*high_range]
+    X_unwr = wr_im[point[0]-low_range+high_range:point[0]+2*high_range,point[1]-low_range+high_range:point[1]+2*high_range]
+    unwr_im = unwr_im[point[0]-low_range+high_range:point[0]+2*high_range,point[1]-low_range+high_range:point[1]+2*high_range]
+    checked_binary_im = checked_binary_im[point[0]-low_range+high_range:point[0]+2*high_range,point[1]-low_range+high_range:point[1]+2*high_range]
     
     #Create a list of indicies in phi_unwr that have been unwrapped
-    phi_unwr_data = numpy.ndarray.flatten(phi_unwr_data)
+    checked_binary_im = numpy.ndarray.flatten(checked_binary_im)
     X_unwr = numpy.ndarray.flatten(X_unwr)
-    unwr_indicies = numpy.where(numpy.ndarray.flatten(phi_unwr_data) != 100)[0]
+    unwr_im = numpy.ndarray.flatten(unwr_im)
+    unwr_indicies = numpy.where(numpy.ndarray.flatten(checked_binary_im) != 0)[0]
     
     #Get values of the wrapped and unwrapped phase, put into arrays called phi_unwr and X
-    phi_unwr = numpy.empty(1,len(unwr_indicies))
-    X = numpy.empty(1,len(unwr_indicies))
+    X = numpy.empty((1,len(unwr_indicies)))
+    phi_unwr = numpy.empty((1,len(unwr_indicies)))
     for i in range (0,len(unwr_indicies)):
-        phi_unwr[i] = phi_unwr_data[unwr_indicies[i]]
+        phi_unwr[i] = unwr_im[unwr_indicies[i]]
         X[i] = X_unwr[unwr_indicies[i]]
                   
     #Find B, E, rank, and singular values of X
     [B, E, rank, s] = numpy.linalg.lstsq(X,phi_unwr)
                   
     #Estimate the value of phi using the least squares equation
-    point_val = original_im_data[point[0]+high_range][point[1]+high_range]
-    phi_est = numpy.matmul(point_val,B)
+    point_val = wr_im[point[0]+high_range][point[1]+high_range]
+    phi_est = point_val*B
     
     #Set the value of phi according to the original image data
-    phi = original_im_data[point[0],point[1]]
+    phi = wr_im[point[0]+high_range][point[1]+high_range]
                   
     #Calculate the unwrapped phase using ***insert forumula***
     unwr_phase = phi + 2 * pi * int((phi_est - phi)/(2 * pi))
